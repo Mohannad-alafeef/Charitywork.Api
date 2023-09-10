cce85fed848da7eb34d25242f63852b45ecb4b6d
mohannad <mohannadafif1998@gmail.com>
2023-09-10 16:45:01 +0300

    Role ready

---
+++ CharityWork.Core/Repository/IRoleRepository.cs
@@ -0,0 +1,17 @@
+﻿using CharityWork.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharityWork.Core.Repository {
+	public interface IRoleRepository {
+
+		 void createRole(Role role);
+		 Task<IEnumerable<Role>> getRoles();
+		 Task<Role> getRoleById(int id);
+		 void updateRole(Role role);
+		 void deleteRole(int id);
+	}
+}
---
+++ Charitywork.Api/Controllers/RoleController.cs
@@ -0,0 +1,38 @@
+﻿using CharityWork.Core.Models;
+using CharityWork.Core.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CharityWork.Api.Controllers {
+	[Route("api/[controller]")]
+	[ApiController]
+	public class RoleController : ControllerBase {
+		private readonly IRoleService _roleService;
+
+		public RoleController(IRoleService roleService) {
+			_roleService = roleService;
+		}
+
+		[HttpPost]
+		public void createRole(Role role) {
+			_roleService.createRole(role);
+		}
+		[HttpGet]
+		public Task<IEnumerable<Role>> getRoles() {
+			return _roleService.getRoles();
+		}
+		[HttpGet("{id}")]
+		public Task<Role> getRoleById(int id) {
+			return _roleService.getRoleById(id);
+		}
+		[HttpPost("update")]
+		public void updateRole(Role role) {
+			_roleService.updateRole(role);
+		}
+		[HttpPost("delete/{id}")]
+		public void deleteRole(int id) {
+			_roleService.deleteRole(id);
+		}
+
+	}
+}
