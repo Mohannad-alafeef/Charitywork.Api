2230e0dca64062cd324f75cc6639e5820851df4a
mohannad <mohannadafif1998@gmail.com>
2023-09-13 19:45:55 +0300

    Contact Ready

---
+++ CharityWork.Core/Repository/IContactRepository.cs
@@ -0,0 +1,16 @@
+﻿using CharityWork.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharityWork.Core.Repository {
+	public interface IContactRepository {
+		void CreateContact(Contact contact);
+		Task<IEnumerable<Contact>> GetAll();
+		void DeleteContact(int id);
+		void ChangeStatus(Contact contact);
+
+	}
+}
---
+++ Charitywork.Api/Controllers/ContactController.cs
@@ -0,0 +1,33 @@
+﻿using CharityWork.Core.Models;
+using CharityWork.Core.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CharityWork.Api.Controllers {
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ContactController : ControllerBase {
+		private readonly IContactService _contactService;
+
+		public ContactController(IContactService contactService) {
+			_contactService = contactService;
+		}
+
+		[HttpPut]
+		public void ChangeStatus(Contact contact) {
+			_contactService.ChangeStatus(contact);
+		}
+		[HttpPost]
+		public void CreateContact(Contact contact) {
+			_contactService.CreateContact(contact);
+		}
+		[HttpDelete("Delete/{id}")]
+		public void DeleteContact(int id) {
+			_contactService.DeleteContact(id);
+		}
+		[HttpGet]
+		public Task<IEnumerable<Contact>> GetAll() {
+			return _contactService.GetAll();
+		}
+	}
+}
