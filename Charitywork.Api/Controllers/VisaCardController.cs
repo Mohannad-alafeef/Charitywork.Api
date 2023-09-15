4e5c2e9ff1dc9a4ae03c39e77da1aedebe598863
mohannad <mohannadafif1998@gmail.com>
2023-09-15 20:56:27 +0300

    final project api v1

--- Charitywork.Api/Controllers/VisaCardController.cs
+++ Charitywork.Api/Controllers/VisaCardController.cs
@@ -3,44 +3,41 @@ using CharityWork.Core.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace CharityWork.Api.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class VisaCardController : ControllerBase
-    {
-        private readonly IVisaCardService _visaCardService;
+namespace CharityWork.Api.Controllers {
+	[Route("api/[controller]")]
+	[ApiController]
+	public class VisaCardController : ControllerBase {
+		private readonly IVisaCardService _visaCardService;
 
-        public VisaCardController(IVisaCardService visaCardService)
-        {
+		public VisaCardController(IVisaCardService visaCardService) {
 
-            _visaCardService = visaCardService;
-        }
+			_visaCardService = visaCardService;
+		}
 
-        [HttpPost]
-        public void createVisaCard(VisaCard visaCard)
-        {
-            _visaCardService.createVisaCard(visaCard);
-        }
-        [HttpPut]
-        public void updateVisaCard(VisaCard visaCard)
-        {
-            _visaCardService.updateVisaCard(visaCard);
-        }
-        [HttpDelete("{id}")]
-        public void deleteVisaCard(int id)
-        {
-            _visaCardService.deleteVisaCard(id);
-        }
-        [HttpGet]
-        public Task<IEnumerable<VisaCard>> allVisaCard()
-        {
-            return _visaCardService.allVisaCard();
-        }
-        [HttpGet("{id}")]
-        public VisaCard getVisaCard(int id)
-        {
-            return _visaCardService.getVisaCard(id);
-        }
-    }
+		[HttpPost]
+		public void createVisaCard(VisaCard visaCard) {
+			_visaCardService.createVisaCard(visaCard);
+		}
+		[HttpPut]
+		public void updateVisaCard(VisaCard visaCard) {
+			_visaCardService.updateVisaCard(visaCard);
+		}
+		[HttpDelete("{id}")]
+		public void deleteVisaCard(int id) {
+			_visaCardService.deleteVisaCard(id);
+		}
+		[HttpGet]
+		public Task<IEnumerable<VisaCard>> allVisaCard() {
+			return _visaCardService.allVisaCard();
+		}
+		[HttpGet("{id}")]
+		public IActionResult getVisaCard(int id) {
+			var visa = _visaCardService.getVisaCard(id);
+			if (visa == null)
+				return NotFound();
+			else
+
+				return Ok(visa);
+		}
+	}
 }
